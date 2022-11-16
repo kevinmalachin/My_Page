2b72840d2928d6893d4836187e3e28b842a65c09
Kevin Malachin <kevinmalachin@gmail.com>
2022-11-16 17:08:17 +0100

    Appunti

--- JavaScript.js
+++ JavaScript.js
@@ -1,3 +1,6 @@
+function conversioneSessantesimiInCentesimi() {
+
 let centesimi = 120.64; //variabile per i centesimi
 let sessantesimi = centesimi * (60 / 100); //converto i centesimi in sessantesimi
 console.log(`il valore in sessantesimi è ${sessantesimi}`);
+}
