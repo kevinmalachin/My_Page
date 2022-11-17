e5cd4c5ae2ce878ffaf695f343807768978e38ab
Kevin Malachin <kevinmalachin@gmail.com>
2022-11-17 16:26:49 +0100

    Appunti

--- JavaScript.js
+++ JavaScript.js
@@ -1,5 +1,5 @@
 function centesimiInSessantesimi(){
     let input = prompt("Inserisci il valore in centesimi.");
     if(input!="") document.write(`Il valore in sessantesimi è ${input * (60/100)}`);
-     else console.log("vuoto!");
+     else document.write("Vuoto!");
 }
