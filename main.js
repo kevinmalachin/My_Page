1ff21fc40eb2022faaf91d6025e84a89826e2861
Kevin Malachin <kevinmalachin@gmail.com>
2024-02-04 00:54:07 +0100

    Appunti

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadFile("index.html");
+
+  // Aperto DevTools (commentare nella versione di produzione)
+  // mainWindow.webContents.openDevTools();
+
+  mainWindow.on("closed", function () {
+    app.quit();
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.on("activate", function () {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
